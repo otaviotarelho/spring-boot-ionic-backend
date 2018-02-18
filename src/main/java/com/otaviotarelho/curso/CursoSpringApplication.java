1632fd186c47eafa52e941c2572a0439c89e4d64
otaviotarelhodb <otarelho@ucdavis.edu>
2018-02-18 20:09:20 -0300

    Criando operação de instanciação

--- src/main/java/com/otaviotarelho/curso/CursoSpringApplication.java
+++ src/main/java/com/otaviotarelho/curso/CursoSpringApplication.java
@@ -1,12 +1,30 @@
 package com.otaviotarelho.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringApplication {
+import com.otaviotarelho.curso.domain.Categoria;
+import com.otaviotarelho.curso.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoSpringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
