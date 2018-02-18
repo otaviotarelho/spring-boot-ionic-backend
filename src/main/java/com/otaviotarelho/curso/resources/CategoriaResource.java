9520e6f6be4b93815348a81359d61a0f3ff40738
otaviotarelhodb <otarelho@ucdavis.edu>
2018-02-18 19:19:37 -0300

    Teste primeira classe de dominio

--- src/main/java/com/otaviotarelho/curso/resources/CategoriaResource.java
+++ src/main/java/com/otaviotarelho/curso/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.otaviotarelho.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.otaviotarelho.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria categoria = new Categoria(1, "informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 }
