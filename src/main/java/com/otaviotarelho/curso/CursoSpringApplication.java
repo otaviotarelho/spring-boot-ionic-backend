46a8394f5addb5dd2f48026ed0e1dd5599f0264b
otaviotarelhodb <otarelho@ucdavis.edu>
2018-02-18 18:46:55 -0300

    Projeto criado

---
+++ src/main/java/com/otaviotarelho/curso/CursoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.otaviotarelho.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+}
