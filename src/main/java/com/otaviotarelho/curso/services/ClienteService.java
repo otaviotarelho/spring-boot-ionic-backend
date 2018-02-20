cbb31128115929e622ec0cae72fcdc67c73dc849
otaviotarelhodb <otarelho@ucdavis.edu>
2018-02-19 22:14:09 -0300

    Endpoint cliente/{id}

---
+++ src/main/java/com/otaviotarelho/curso/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.otaviotarelho.curso.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.otaviotarelho.curso.domain.Cliente;
+import com.otaviotarelho.curso.repositories.ClienteRepository;
+import com.otaviotarelho.curso.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("object não encontrado");
+		}
+		
+		return obj;
+	}
+}
