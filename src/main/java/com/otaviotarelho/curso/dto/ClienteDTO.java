d1655996605181f39aaf6cb0f5035a127f08ee7f
otaviotarelhodb <otarelho@ucdavis.edu>
2018-02-25 19:36:28 -0300

    PUT, DELETE, GET for Cliente

---
+++ src/main/java/com/otaviotarelho/curso/dto/ClienteDTO.java
@@ -0,0 +1,57 @@
+package com.otaviotarelho.curso.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.otaviotarelho.curso.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento do Nome obrigatório.")
+	@Length(min=5, max=120, message="O tamanho do nome deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento do E-mail obrigatório.")
+	@Email(message="E-mail inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
--- src/main/java/com/otaviotarelho/curso/services/ClienteService.java
+++ src/main/java/com/otaviotarelho/curso/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.otaviotarelho.curso.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.otaviotarelho.curso.domain.Cliente;
+import com.otaviotarelho.curso.dto.ClienteDTO;
 import com.otaviotarelho.curso.repositories.ClienteRepository;
+import com.otaviotarelho.curso.services.exceptions.DataIntegrityException;
 import com.otaviotarelho.curso.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,5 +29,44 @@ public class ClienteService {
 		}
 		
 		return obj;
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);			
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linePerPage, String orderBy, String direction){
+		PageRequest pageResquest = new PageRequest(page, linePerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageResquest);
+	}
+	
+	public Cliente formDTO(ClienteDTO objDTO) {
+		
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
