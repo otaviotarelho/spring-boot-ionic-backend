c72e5fbfaaf864cee56c97c9806637c4ec14aea2
otaviotarelhodb <otarelho@ucdavis.edu>
2018-02-25 20:52:31 -0300

    Custom Validation: unique e-mail

--- src/main/java/com/otaviotarelho/curso/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/otaviotarelho/curso/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.otaviotarelho.curso.domain.Cliente;
 import com.otaviotarelho.curso.domain.enums.TipoCliente;
 import com.otaviotarelho.curso.dto.ClienteNewDTO;
+import com.otaviotarelho.curso.repositories.ClienteRepository;
 import com.otaviotarelho.curso.resources.exceptions.FieldMessage;
 import com.otaviotarelho.curso.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(cliente != null) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
 		}
 		
 		for (FieldMessage e : list) {
