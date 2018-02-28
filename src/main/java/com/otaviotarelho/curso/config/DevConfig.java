9d99b84c5b654e4f24c56f87f9d94425251b2dbf
otaviotarelhodb <otarelho@ucdavis.edu>
2018-02-27 22:03:13 -0300

    Application Development Profile

--- src/main/java/com/otaviotarelho/curso/config/DevConfig.java
+++ src/main/java/com/otaviotarelho/curso/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.otaviotarelho.curso.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +11,22 @@ import org.springframework.context.annotation.Profile;
 import com.otaviotarelho.curso.services.DatabaseService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DatabaseService databaseService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			 return false;
+		}
+		
 		databaseService.instantiateDatabase();
 		return true;
 	}
