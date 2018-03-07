b39ecdf0836cceebc7f9b821917abfd0dc172a53
otaviotarelhodb <otarelho@ucdavis.edu>
2018-03-06 21:01:10 -0300

    Authorization

--- src/main/java/com/otaviotarelho/curso/config/SecurityConfig.java
+++ src/main/java/com/otaviotarelho/curso/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.otaviotarelho.curso.security.JWTAuthenticationFilter;
+import com.otaviotarelho.curso.security.JWTAuthorizationFilter;
 import com.otaviotarelho.curso.security.JWTUtil;
 
 @Configuration
@@ -47,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
