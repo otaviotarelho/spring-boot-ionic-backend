6ce39aee74e6bcd9668c7ac00c5b301128ec74dc
otaviotarelhodb <otarelho@ucdavis.edu>
2018-03-08 22:19:18 -0300

    S3 Configuration

--- src/main/java/com/otaviotarelho/curso/CursoSpringApplication.java
+++ src/main/java/com/otaviotarelho/curso/CursoSpringApplication.java
@@ -1,13 +1,17 @@
 package com.otaviotarelho.curso;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.otaviotarelho.curso.services.S3Service;
+
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -15,6 +19,6 @@ public class CursoSpringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		
+		s3Service.uploadile("/Users/otaviortbarros/Downloads/s2notx.jpg");
 	}
 }
---
+++ src/main/java/com/otaviotarelho/curso/services/S3Service.java
@@ -0,0 +1,39 @@
+package com.otaviotarelho.curso.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+	
+	private Logger log = LoggerFactory.getLogger(S3Service.class);
+	
+	@Autowired
+	private AmazonS3 s3Client;
+	
+	@Value("${s3.bucket}")
+	private String bucketName;
+	
+	public void uploadile(String localFilePath) {
+		try {
+			File file = new File(localFilePath);
+			s3Client.putObject(new PutObjectRequest(bucketName, "testando.jpg", file));
+			log.info("Upload finalizado");
+		} catch (AmazonServiceException e) {
+			log.info("AmazonServiceException >> " + e.getErrorMessage());
+			log.info("AmazonServiceException >> " + e.getErrorCode());
+		} catch (AmazonClientException e) {
+			log.info("AmazonClientException >> " + e.getMessage());
+		}
+	}
+}
