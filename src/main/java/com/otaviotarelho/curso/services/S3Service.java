4bf776ffc8944bfecc22386a8eecfc966893542a
otaviotarelhodb <otarelho@ucdavis.edu>
2018-03-11 19:56:47 -0300

    Amazon Exceptions Handler

--- src/main/java/com/otaviotarelho/curso/services/S3Service.java
+++ src/main/java/com/otaviotarelho/curso/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.otaviotarelho.curso.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -35,7 +36,7 @@ public class S3Service {
 
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro IO >> " + e.getMessage());
+			throw new FileException("Erro IO >> " + e.getMessage());
 		}
 	}
 
@@ -47,7 +48,7 @@ public class S3Service {
 			log.info("Upload finalizado");
 			return s3Client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao conectar");
+			throw new FileException("Erro ao conectar");
 		}
 	}
 
